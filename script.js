c897f6f64b4e6880bb551b7ff15f62f8d676193b
Jerod Wilhoit <jwilhoit83@gmail.com>
2021-10-27 21:21:30 -0500

    Score change

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const teamPoints = [
   { dallas: '0' },
   { detroit: '0' },
   { edmonton: '0' },
-  { florida: '6' },
+  { florida: '7' },
   { vegas: '0' },
   { 'los-angeles': '0' },
   { minnesota: '0' },
