88533dfc63dab3c7452ca938107e38e67a8ccd1b
Jerod Wilhoit <jwilhoit83@gmail.com>
2022-01-05 22:16:48 -0600

    added upcoming game card to page using NHL API

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+// object for the team IDs on the NHL API
+const teamIds = {
+  anaheim: 24,
+  arizona: 53,
+  boston: 6,
+  buffalo: 7,
+  calgary: 20,
+  carolina: 12,
+  chicago: 16,
+  colorado: 21,
+  columbus: 29,
+  dallas: 25,
+  detroit: 17,
+  edmonton: 22,
+  florida: 13,
+  vegas: 54,
+  'los-angeles': 26,
+  minnesota: 30,
+  montreal: 8,
+  nashville: 18,
+  'new-jersey': 1,
+  'ny-islanders': 2,
+  'ny-rangers': 3,
+  ottawa: 9,
+  philadelphia: 4,
+  pittsburgh: 5,
+  'san-jose': 28,
+  seattle: 55,
+  'st-louis': 19,
+  tampa: 14,
+  toronto: 10,
+  vancouver: 23,
+  washington: 15,
+  winnipeg: 52,
+}
+
+const currentChamp = 'florida'
+
+let days = 10
+let dateCurrent = new Date()
+let dateTemp = new Date().setDate(new Date().getDate() + days)
+let dateFuture = new Date(dateTemp)
+
+function formatDate(date) {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return [year, month.toString().padStart(2, '0'), day.toString().padStart(2, '0')].join('-')
+}
+
+// grabbing the upcoming schedule for the current cup holder and adding date and logos to the DOM
+fetch(
+  `https://statsapi.web.nhl.com/api/v1/schedule?teamId=${
+    teamIds[currentChamp]
+  }&startDate=${formatDate(dateCurrent)}&endDate=${formatDate(dateFuture)}`
+)
+  .then(res => res.json())
+  .then(res => {
+
+    // formatting the date of next game correctly
+    let gameDate = res.dates[0].date
+    gameDate = gameDate.split('-')
+    let gameTime = new Date()
+    gameTime.setFullYear(gameDate[0])
+    gameTime.setDate(gameDate[2])
+    gameTime.setMonth(gameDate[1] - 1)
+
+    // formatting the home and away team names correctly
+    let homeTeam = res.dates[0].games[0].teams.home.team.name
+    let awayTeam = res.dates[0].games[0].teams.away.team.name
+    homeTeam = homeTeam.split(' ')[homeTeam.split(' ').length - 1].toLowerCase()
+    awayTeam = awayTeam.split(' ')[awayTeam.split(' ').length - 1].toLowerCase()
+
+    // adding date and team logos for the next game
+    document.getElementById('date').innerText = gameTime.toDateString()
+    document.getElementById('home').setAttribute('src', `logos/${homeTeam}.png`)
+    document.getElementById('away').setAttribute('src', `logos/${awayTeam}.png`)
+  })
+
 const teamPoints = [
   { anaheim: '0' },
   { arizona: '0' },
@@ -33,8 +113,6 @@ const teamPoints = [
   { winnipeg: '2' },
 ]
 
-const currentChamp = 'florida'
-
 const james = document.querySelector('.james')
 const jerod = document.querySelector('.jerod')
 const jeff = document.querySelector('.jeff')
@@ -45,7 +123,7 @@ const players = [james, jerod, jeff, billy]
 // iterates through team points array and displays all team points accordingly
 
 teamPoints.forEach(team => {
-  teamName = Object.keys(team)[0]
+  const teamName = Object.keys(team)[0]
   document.getElementById(teamName).innerText = team[teamName]
 })
 
