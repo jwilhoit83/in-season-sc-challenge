f2202b635c7854379968cda13e3905b315ce2fec
Jerod Wilhoit <jwilhoit83@gmail.com>
2022-04-14 08:22:09 -0500

    Score change

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const teamPoints = [
   { calgary: '0' },
   { carolina: '1' },
   { chicago: '0' },
-  { colorado: '8' },
+  { colorado: '9' },
   { columbus: '2' },
   { dallas: '7' },
   { detroit: '1' },
