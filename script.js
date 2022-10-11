05109f54a3d0fc8c79f57558fd5d1a551fb5d62d
Jerod Wilhoit <jwilhoit83@gmail.com>
2022-10-10 22:23:37 -0500

    updated player teams and points after draft

--- script.js
+++ script.js
@@ -14,39 +14,43 @@ const teamIds = {
   edmonton: 22,
   florida: 13,
   vegas: 54,
-  'los-angeles': 26,
+  "los-angeles": 26,
   minnesota: 30,
   montreal: 8,
   nashville: 18,
-  'new-jersey': 1,
-  'ny-islanders': 2,
-  'ny-rangers': 3,
+  "new-jersey": 1,
+  "ny-islanders": 2,
+  "ny-rangers": 3,
   ottawa: 9,
   philadelphia: 4,
   pittsburgh: 5,
-  'san-jose': 28,
+  "san-jose": 28,
   seattle: 55,
-  'st-louis': 19,
+  "st-louis": 19,
   tampa: 14,
   toronto: 10,
   vancouver: 23,
   washington: 15,
   winnipeg: 52,
-}
+};
 
-const currentChamp = 'colorado'
+const currentChamp = "colorado";
 
-let days = 20
-let dateCurrent = new Date()
-let dateTemp = new Date().setDate(new Date().getDate() + days)
-let dateFuture = new Date(dateTemp)
+let days = 20;
+let dateCurrent = new Date();
+let dateTemp = new Date().setDate(new Date().getDate() + days);
+let dateFuture = new Date(dateTemp);
 
 function formatDate(date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
 
-  return [year, month.toString().padStart(2, '0'), day.toString().padStart(2, '0')].join('-')
+  return [
+    year,
+    month.toString().padStart(2, "0"),
+    day.toString().padStart(2, "0"),
+  ].join("-");
 }
 
 // grabbing the upcoming schedule for the current cup holder and adding date and logos to the DOM
@@ -55,83 +59,94 @@ fetch(
     teamIds[currentChamp]
   }&startDate=${formatDate(dateCurrent)}&endDate=${formatDate(dateFuture)}`
 )
-  .then(res => res.json())
-  .then(res => {
-    const game = res.dates[0].games[0]
+  .then((res) => res.json())
+  .then((res) => {
+    const game = res.dates[0].games[0];
 
     // formatting the home and away team names correctly
-    let homeTeam = game.teams.home.team.name
-    let awayTeam = game.teams.away.team.name
-    homeTeam = homeTeam.split(' ')[homeTeam.split(' ').length - 1].toLowerCase()
-    awayTeam = awayTeam.split(' ')[awayTeam.split(' ').length - 1].toLowerCase()
+    let homeTeam = game.teams.home.team.name;
+    let awayTeam = game.teams.away.team.name;
+    homeTeam = homeTeam
+      .split(" ")
+      [homeTeam.split(" ").length - 1].toLowerCase();
+    awayTeam = awayTeam
+      .split(" ")
+      [awayTeam.split(" ").length - 1].toLowerCase();
 
     // adding date and team logos for the next game
-    document.getElementById('date').innerText = new Date(game.gameDate).toLocaleString('en-US', {
-      weekday: 'long',
-      month: 'short',
-      day: 'numeric',
-    })
-    document.getElementById('time').innerText = new Date(game.gameDate).toLocaleString('en-US', {
-      hour: 'numeric',
-      minute: 'numeric',
-    })
-    document.getElementById('home').setAttribute('src', `logos/${homeTeam}.png`)
-    document.getElementById('away').setAttribute('src', `logos/${awayTeam}.png`)
-  })
+    document.getElementById("date").innerText = new Date(
+      game.gameDate
+    ).toLocaleString("en-US", {
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+    });
+    document.getElementById("time").innerText = new Date(
+      game.gameDate
+    ).toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+    });
+    document
+      .getElementById("home")
+      .setAttribute("src", `logos/${homeTeam}.png`);
+    document
+      .getElementById("away")
+      .setAttribute("src", `logos/${awayTeam}.png`);
+  });
 
 const teamPoints = [
-  { anaheim: '0' },
-  { arizona: '0' },
-  { boston: '0' },
-  { buffalo: '0' },
-  { calgary: '0' },
-  { carolina: '0' },
-  { chicago: '0' },
-  { colorado: '0' },
-  { columbus: '0' },
-  { dallas: '0' },
-  { detroit: '0' },
-  { edmonton: '0' },
-  { florida: '0' },
-  { vegas: '0' },
-  { 'los-angeles': '0' },
-  { minnesota: '0' },
-  { montreal: '0' },
-  { nashville: '0' },
-  { 'new-jersey': '0' },
-  { 'ny-islanders': '0' },
-  { 'ny-rangers': '0' },
-  { ottawa: '0' },
-  { philadelphia: '0' },
-  { pittsburgh: '0' },
-  { 'san-jose': '0' },
-  { seattle: '0' },
-  { 'st-louis': '0' },
-  { tampa: '0' },
-  { toronto: '0' },
-  { vancouver: '0' },
-  { washington: '0' },
-  { winnipeg: '0' },
-]
+  { anaheim: "0" },
+  { arizona: "0" },
+  { boston: "0" },
+  { buffalo: "0" },
+  { calgary: "0" },
+  { carolina: "0" },
+  { chicago: "0" },
+  { colorado: "1" },
+  { columbus: "0" },
+  { dallas: "0" },
+  { detroit: "0" },
+  { edmonton: "0" },
+  { florida: "0" },
+  { vegas: "0" },
+  { "los-angeles": "0" },
+  { minnesota: "0" },
+  { montreal: "0" },
+  { nashville: "0" },
+  { "new-jersey": "0" },
+  { "ny-islanders": "0" },
+  { "ny-rangers": "0" },
+  { ottawa: "0" },
+  { philadelphia: "0" },
+  { pittsburgh: "0" },
+  { "san-jose": "0" },
+  { seattle: "0" },
+  { "st-louis": "0" },
+  { tampa: "0" },
+  { toronto: "0" },
+  { vancouver: "0" },
+  { washington: "0" },
+  { winnipeg: "0" },
+];
 
-const james = document.querySelector('.james')
-const jerod = document.querySelector('.jerod')
-const jeff = document.querySelector('.jeff')
-const billy = document.querySelector('.billy')
+const james = document.querySelector(".james");
+const jerod = document.querySelector(".jerod");
+const jeff = document.querySelector(".jeff");
+const billy = document.querySelector(".billy");
 
-const players = [james, jerod, jeff, billy]
+const players = [james, jerod, jeff, billy];
 
 // iterates through team points array and displays all team points accordingly
 
-teamPoints.forEach(team => {
-  const teamName = Object.keys(team)[0]
-  document.getElementById(teamName).innerText = team[teamName]
-})
+teamPoints.forEach((team) => {
+  const teamName = Object.keys(team)[0];
+  document.getElementById(teamName).innerText = team[teamName];
+});
 
 // adds a border to the active cup champ and changes theme to team colors
 
-document.getElementById(currentChamp).parentElement.classList.add('champ')
-
+document.getElementById(currentChamp).parentElement.classList.add("champ");
 
 // document.documentElement.className = currentChamp
 
@@ -140,15 +155,15 @@ document.getElementById(currentChamp).parentElement.classList.add('champ')
 
 // gets all team points for each player and displays the max value of each next to the player name
 
-getPoints()
+getPoints();
 
 function getPoints() {
-  players.forEach(player => {
-    const playerArray = []
-    player.querySelectorAll('.team-score').forEach(team => {
-      playerArray.push(Number(team.innerText))
-    })
+  players.forEach((player) => {
+    const playerArray = [];
+    player.querySelectorAll(".team-score").forEach((team) => {
+      playerArray.push(Number(team.innerText));
+    });
 
-    player.querySelector('#high-points').innerText = Math.max(...playerArray)
-  })
+    player.querySelector("#high-points").innerText = Math.max(...playerArray);
+  });
 }
