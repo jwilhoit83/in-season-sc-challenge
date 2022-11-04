7df0945b3302a99db5f87b908dd9687ed6ba8e49
Jerod Wilhoit <jwilhoit83@gmail.com>
2022-11-03 22:52:50 -0500

    Score change

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const teamPoints = [
   { minnesota: "0" },
   { montreal: "0" },
   { nashville: "0" },
-  { "new-jersey": "1" },
+  { "new-jersey": "2" },
   { "ny-islanders": "0" },
   { "ny-rangers": "0" },
   { ottawa: "0" },
