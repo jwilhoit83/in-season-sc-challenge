0a17aa895bb5a18d09b59ca5fdf73e7298720b82
Jerod Wilhoit <jwilhoit83@gmail.com>
2022-12-03 21:59:45 -0600

    Score change

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const teamIds = {
   winnipeg: 52,
 };
 
-const currentChamp = "toronto";
+const currentChamp = "tampa";
 
 let days = 20;
 let dateCurrent = new Date();
@@ -123,7 +123,7 @@ const teamPoints = [
   { "san-jose": "0" },
   { seattle: "1" },
   { "st-louis": "0" },
-  { tampa: "0" },
+  { tampa: "1" },
   { toronto: "5" },
   { vancouver: "2" },
   { washington: "0" },
