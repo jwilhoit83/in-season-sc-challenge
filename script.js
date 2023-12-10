9dda1b4bf2c6bca72e66fae3e0843bc84ca42c3a
Jerod Wilhoit <jwilhoit83@gmail.com>
2023-12-09 22:40:48 -0600

    score change

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ const teamPoints = [
   { "ny-islanders": "0" },
   { "ny-rangers": "1" },
   { ottawa: "0" },
-  { philadelphia: "1" },
+  { philadelphia: "2" },
   { pittsburgh: "4" },
   { "san-jose": "0" },
   { seattle: "0" },
