ba321744480d6da53132488df5d91ad28883ea55
Jerod Wilhoit <jwilhoit83@gmail.com>
2024-01-22 21:05:48 -0600

    score change

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ fetch('23-24-schedule.json')
 const teamPoints = [
   { anaheim: "2" },
   { arizona: "6" },
-  { boston: "3" },
+  { boston: "4" },
   { buffalo: "0" },
   { calgary: "0" },
   { carolina: "0" },
