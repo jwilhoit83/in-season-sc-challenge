b7f2361614737a214a3494f5f364d01f811d7b5c
Jerod Wilhoit <jwilhoit83@gmail.com>
2024-02-07 14:35:38 -0600

    score change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "carolina";
+const currentChamp = "vancouver";
 
 let nextHome = ''
 let nextVisitor = ''
@@ -106,7 +106,7 @@ const teamPoints = [
   { "st-louis": "2" },
   { tampa: "0" },
   { toronto: "0" },
-  { vancouver: "1" },
+  { vancouver: "2" },
   { washington: "0" },
   { winnipeg: "0" },
 ];
