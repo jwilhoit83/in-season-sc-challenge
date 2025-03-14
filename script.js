e379127a74615acbb3c6a6986b1d3493183ac909
Jerod Wilhoit <jwilhoit83@gmail.com>
2025-03-14 07:39:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "boston";
+const currentChamp = "ottawa";
 
 let nextHome = ''
 let nextVisitor = ''
@@ -97,7 +97,7 @@ const teamPoints = [
   { "new-jersey": "0" },
   { "ny-islanders": "0" },
   { "ny-rangers": "1" },
-  { ottawa: "3" },
+  { ottawa: "4" },
   { philadelphia: "0" },
   { pittsburgh: "2" },
   { "san-jose": "2" },
