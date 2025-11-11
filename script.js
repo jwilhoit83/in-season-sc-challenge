d237b4711133a54b3cfc1bf512be38017a873405
Jerod Wilhoit <jwilhoit83@gmail.com>
2025-11-11 04:22:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ const teamPoints = [
   { "new-jersey": "0" },
   { "ny-islanders": "0" },
   { "ny-rangers": "0" },
-  { ottawa: "1" },
+  { ottawa: "2" },
   { philadelphia: "3" },
   { pittsburgh: "0" },
   { "san-jose": "0" },
