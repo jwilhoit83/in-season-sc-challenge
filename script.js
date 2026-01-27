c6ae616731cbfb0a8ec39212980d56a5982da62e
Jerod Wilhoit <jwilhoit83@gmail.com>
2026-01-27 04:23:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "utah";
+const currentChamp = "tampa";
 
 let currentChallenger = "";
 let nextHome = "";
@@ -110,7 +110,7 @@ const teamPoints = [
   { "san-jose": "0" },
   { seattle: "2" },
   { "st-louis": "1" },
-  { tampa: "1" },
+  { tampa: "2" },
   { toronto: "4" },
   { utah: "5" },
   { vancouver: "0" },
