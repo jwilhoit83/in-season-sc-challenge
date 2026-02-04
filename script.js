bc755ed3ebcb6d19b3381b860edb8320f6d0d361
Jerod Wilhoit <jwilhoit83@gmail.com>
2026-02-04 09:17:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const teamPoints = [
   { "san-jose": "0" },
   { seattle: "2" },
   { "st-louis": "1" },
-  { tampa: "4" },
+  { tampa: "5" },
   { toronto: "4" },
   { utah: "5" },
   { vancouver: "0" },
