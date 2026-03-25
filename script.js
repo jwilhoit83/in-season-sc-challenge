9a926ecf661b9771c561ecdf5067db3e7962d8a0
Jerod Wilhoit <jwilhoit83@gmail.com>
2026-03-25 04:22:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const teamPoints = [
   { calgary: "0" },
   { carolina: "2" },
   { chicago: "0" },
-  { colorado: "11" },
+  { colorado: "12" },
   { columbus: "0" },
   { dallas: "3" },
   { detroit: "0" },
