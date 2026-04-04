2931ec8a63868b9e04746318d5c57bb81f554343
Jerod Wilhoit <jwilhoit83@gmail.com>
2026-04-04 17:07:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "dallas";
+const currentChamp = "colorado";
 
 let currentChallenger = "";
 let nextHome = "";
@@ -90,7 +90,7 @@ const teamPoints = [
   { calgary: "0" },
   { carolina: "2" },
   { chicago: "0" },
-  { colorado: "13" },
+  { colorado: "14" },
   { columbus: "0" },
   { dallas: "4" },
   { detroit: "0" },
